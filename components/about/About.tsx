5df3bbc0cc003c77176ec842688ec1d41285081e
Edgar Alejandro Santiago Perez <alejandrosantiago.cfeteit@gmail.com>
2024-12-11 19:57:00 -0600

    🍱 add assets

--- components/about/About.tsx
+++ components/about/About.tsx
@@ -1,3 +1,4 @@
+import { Divider } from "@nextui-org/react";
 import AboutDescription from "./AboutDescription";
 import ProfileImg from "./ProfileImg";
 
@@ -6,6 +7,7 @@ const About = () => {
     <div className="min-h-screen flex flex-col items-center justify-center">
       <ProfileImg />
       <AboutDescription />
+      <Divider className="bg-[#CC005F] w-4/5 absolute bottom-[-1px]" />
     </div>
   );
 };
