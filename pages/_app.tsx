f23dd0c002a87c9322db58fa3ac609d23f03f6d9
Alejandro Santiago <lesthath_@hotmail.com>
2024-12-15 22:12:38 -0600

    ✨ Add Skills component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,8 +6,9 @@ import { BackgroundBeams } from "@/components/hero/BackgroundBeams";
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <NextUIProvider className="bg-background min-h-screen">
-      <BackgroundBeams className="fixed inset-0 z-1" />
-      <div className="relative min-h-screen">
+      {/* Escalado del fondo con clases responsivas */}
+      <BackgroundBeams className="fixed inset-0 z-1 scale-200 xl:scale-100" />
+      <div className="relative min-h-screen max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8">
         <Component {...pageProps} />
       </div>
     </NextUIProvider>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,16 @@
 import About from "@/components/about/About";
 import Hero from "@/components/hero/Hero";
 import Proyects from "@/components/proyects/Proyects";
-
+import { Divider } from "@nextui-org/react";
 
 export default function Home() {
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col gap-20 max-w-screen-xl mx-auto -mt-20  md:-mt-0 sm:-mt-0 lg:-mt-0 xl:-mt-0 ">
       <Hero />
-      {/* <Divider className="bg-[#CC005F] w-4/5 mx-auto" /> */}
+       <Divider className="bg-[#CC005F] w-4/5 mx-auto" /> 
       <About />
-      {/* <Divider className="bg-[#CC005F] w-4/5 mx-auto" /> */}
-      <Proyects/>
+      <Divider className="bg-[#CC005F] w-4/5 mx-auto" /> 
+      <Proyects />
     </div>
   );
 }
