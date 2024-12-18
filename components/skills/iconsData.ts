734760ba3bddfe3c652bfc086361cd7e1d20963d
Edgar Alejandro Santiago Perez <alejandrosantiago.cfeteit@gmail.com>
2024-12-17 19:28:59 -0600

    ✨ Add Skills data file

--- components/skills/iconsData.ts
+++ components/skills/iconsData.ts
@@ -1,5 +1,27 @@
 export interface imageProps {
   src: string;
+  name: string;
 }
 
-export const images: imageProps[] = [{ src: "/icons/skills/css3.png" }];
+export const images: imageProps[] = [
+  {
+    src: "/icons/skills/htmlIcon.png",
+    name: "HTML",
+  },
+  { src: "/icons/skills/cssIcon.png", 
+    name: "CSS" },
+  { src: "/icons/skills/reactIcon3.png", 
+    name: "REACT" },
+  {
+    src: "/icons/skills/javaScriptIcon.png",
+    name: "JAVA SCRIPT",
+  },
+  { src: "/icons/skills/figmaIcon2.png", 
+    name: "FIGMA" },
+  {
+    src: "/icons/skills/vsCodeIcon.png",
+    name: "VS CODE",
+  },
+  { src: "/icons/skills/gitIcon.png", 
+    name: "GIT" },
+];
