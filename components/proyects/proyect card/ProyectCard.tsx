301585cacb821b16c97495c33f9ad50bae0d2774
Alejandro Santiago <lesthath_@hotmail.com>
2025-01-29 00:16:56 -0600

    ✨ Update ProyectCard component: enhance layout with flexbox, improve hover effects, and adjust structure for better responsiveness

--- components/proyects/proyect card/ProyectCard.tsx
+++ components/proyects/proyect card/ProyectCard.tsx
@@ -30,10 +30,10 @@ const ProyectCard = ({
   links,
 }: ProyectCardProps) => {
   return (
-    <div className="transform scale-90 md:scale-75 lg:scale-90 transition-transform duration-300">
+    <div className="flex transform scale-90 md:scale-75 lg:scale-90 transition-transform duration-300 justify-center">
       <Card
         shadow="none"
-        className="bg-[#2424244f] flex w-full sm:w-[90%] md:w-[90%] lg:w-[90%] xl:w-[90%] border border-[#4A4A4A] p-5 rounded-[50px] transition-transform duration-100 ease-in-out hover:scale-[102%] overflow-hidden"
+        className="bg-[#2424244f] flex w-full sm:w-[90%] md:w-[90%] lg:w-[90%] xl:w-[90%] border border-[#4A4A4A] p-5 rounded-[50px] transition-transform duration-100 ease-in-out hover:scale-[102%] overflow-hidden hover:border-[#CC005F]"
       >
         {/* Contenedor de imagen */}
         <div className="-m-5 rounded-t-[50px] overflow-hidden h-[200px] relative flex">
@@ -56,12 +56,12 @@ const ProyectCard = ({
           </h3>
         </CardHeader>
 
-        <CardBody className="flex flex-col pt-0">
+        <CardBody className="flex flex-col pt-0 flex-grow">
           <p className="font-montserrat text-justify pb-6 text-[18px]">
             {description}
           </p>
 
-          <div className="flex flex-wrap gap-3">
+          <div className="flex flex-wrap gap-3 ">
             {/* Chips */}
             {technologies.map((tech) => (
               <TechnologyChip
@@ -73,7 +73,7 @@ const ProyectCard = ({
             ))}
           </div>
 
-          <div>
+          <div className="flex-col items-end mt-auto">
             <ProyectLink
               codeUrl={links.codeUrl}
               livePreviewUrl={links.livePreviewUrl}
